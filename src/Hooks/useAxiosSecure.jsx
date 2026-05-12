9c3e92e4270e8836580e933a22a467c718b88f9f
sajib-rahman90 <sajibrahman0090@gmail.com>
2026-05-13 02:34:45 +0600

    deployed my project

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,10 +1,46 @@
 import axios from "axios";
+import useAuth from "./useAuth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
   baseURL: "http://localhost:3000",
 });
 
 const useAxiosSecure = () => {
+  const { user, signOutUserFunc } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const reqInterceptor = axiosSecure.interceptors.request.use((config) => {
+      config.headers.Authorization = `Bearer ${user?.accessToken}`;
+      return config;
+    });
+
+    //interseptor response
+    const resInterceptor = axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log(error);
+
+        const statusCode = error.status;
+        if (statusCode === 401 || statusCode === 403) {
+          signOutUserFunc().then(() => {
+            navigate("/login");
+          });
+        }
+
+        return Promise.reject(error);
+      },
+    );
+
+    return () => {
+      axiosSecure.interceptors.request.eject(reqInterceptor);
+      axiosSecure.interceptors.response.eject(resInterceptor);
+    };
+  }, [user, signOutUserFunc, navigate]);
   return axiosSecure;
 };
 
